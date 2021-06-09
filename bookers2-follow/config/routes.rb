f3c3a063d4dc6dc33265ac150a9274adb45f7bb4
kotaro0928 <jotaro.w0101@gmail.com>
2021-06-09 13:50:15 +0000

    [Fix]課題4

--- bookers2-follow/config/routes.rb
+++ bookers2-follow/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
-  devise_for :users
-  resources :users, only: [:show, :index, :edit, :update]
+
   resources :books
-  resources :relationships, only: [:create, :destroy]
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
